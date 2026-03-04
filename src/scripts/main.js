2b22dded049d082eee307004c85cd485d2aaeca3
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-03-04 15:46:40 +0200

    feat: function for column copyPaste

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write your code here
+function createCell(tag, text) {
+  const cell = document.createElement(tag);
+
+  cell.textContent = text;
+
+  return cell;
+}
+
+function doubleUpColumn(columnName, pasteIndex) {
+  const headersArray = Array.from(document.querySelectorAll('thead th'));
+
+  const columnNamePosition = headersArray.findIndex(
+    (th) => th.textContent === columnName,
+  );
+
+  if (columnNamePosition === -1) {
+    return `There are no column named ${columnName} in the table`;
+  }
+
+  const header = document.querySelector('thead tr');
+  const footer = document.querySelector('tfoot tr');
+  const body = document.querySelectorAll('tbody tr');
+
+  header.insertBefore(
+    createCell('th', columnName),
+    header.children[pasteIndex],
+  );
+
+  footer.insertBefore(
+    createCell('th', columnName),
+    footer.children[pasteIndex],
+  );
+
+  body.forEach((row) => {
+    const cellContent =
+      row.getElementsByTagName('td')[columnNamePosition].textContent;
+
+    const newCell = document.createElement('td');
+
+    newCell.textContent = cellContent;
+
+    row.insertBefore(newCell, row.children[pasteIndex]);
+  });
+}
+
+doubleUpColumn('Position', 4);
